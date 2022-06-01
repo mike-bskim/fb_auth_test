d524ba743a053f8c5918d41a83b4f646931fd688
mike.bskim <mike.bskim@gmail.com>
2022-06-01 16:15:51 +0200

    87_AuthState_AuthProvider

---
+++ lib/providers/auth/auth_provider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/foundation.dart';
+import 'package:firebase_auth/firebase_auth.dart' as fb_auth;
+
+import '../../repositories/auth_repo.dart';
+import 'auth_state.dart';
+
+class AuthProvider extends ChangeNotifier {
+  AuthState _state = AuthState.unknown();
+  AuthState get state => _state;
+
+  final AuthRepo authRepo;
+  AuthProvider({
+    required this.authRepo,
+  });
+
+  void update(fb_auth.User? user) {
+    if (user != null) {
+      _state = _state.copyWith(
+        authStatus: AuthStatus.authenticated,
+        user: user,
+      );
+    } else {
+      _state = _state.copyWith(authStatus: AuthStatus.unauthenticated);
+    }
+    debugPrint('authState: $_state');
+    notifyListeners();
+  }
+
+  void signOut() async {
+    await authRepo.signOut();
+  }
+}
