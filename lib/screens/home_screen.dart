2f89ed8630416e0c3ef225baffc30b3ff694f83d
mike.bskim <mike.bskim@gmail.com>
2022-06-03 14:54:10 +0200

    93_HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
+import 'package:fb_auth_test/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/auth/auth_provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,9 +17,52 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async => false,
-      child: const Scaffold(
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text('Home'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                context.read<AuthProvider>().signOut();
+              },
+              icon: const Icon(Icons.exit_to_app),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ProfileScreen()),
+                );
+              },
+              icon: const Icon(Icons.account_circle),
+            ),
+          ],
+        ),
         body: Center(
-          child: Text('Home'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/flutter_favorite.png',
+                    width: 100,
+                  ),
+                  const SizedBox(width: 20.0),
+                  const Text('Provider', style: TextStyle(fontSize: 20.0)),
+                ],
+              ),
+              const SizedBox(height: 30.0),
+              const Text(
+                ' Provider is an awesome\n state management library\n for flutter!',
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ],
+          ),
         ),
       ),
     );
